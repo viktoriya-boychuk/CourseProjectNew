1c0cc20a61a05c3d27ed510a3e18ad1c7c61e663
Viktor Vovk <volkeee1@gmail.com>
2018-03-13 23:37:50 +0200

    Starting the server along with the application in main class

--- CourseProjectServer/src/Application.java
+++ CourseProjectServer/src/Application.java
@@ -1,9 +1,5 @@
 import utils.Logger;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class Application {
     //TODO: Transform logger into Singleton
 
@@ -14,7 +10,9 @@ public class Application {
 
 //        Attaching a small portion of code to the quitting of the application for it to log the time it got terminated
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            Logger.info("Exit", "Application terminated");
+            Logger.logInfo("Exit", "Application terminated");
         }));
+
+        Server.start();
     }
 }
