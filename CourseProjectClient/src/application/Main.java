6499230bc5089af27f62c73deb2152be982cfa15
Viktor Vovk <volkeee1@gmail.com>
2018-03-13 23:37:04 +0200

    Testing the connection with server in tha Main class

--- CourseProjectClient/src/application/Main.java
+++ CourseProjectClient/src/application/Main.java
@@ -1,26 +1,46 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import utils.ServerConnection;
+
+import java.io.IOException;
+import java.net.InetAddress;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root, 400, 400);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ServerConnection client = null;
+        try {
+            client = new ServerConnection(
+                    InetAddress.getByName(args[0]),
+                    Integer.parseInt(args[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\r\nConnected to Server: " + client.getSocket().getInetAddress());
+        try {
+            client.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        launch(args);
+    }
 }
