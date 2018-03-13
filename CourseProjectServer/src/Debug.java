99175c00492e24b3a744d3ba2592868352c756ae
Viktor Vovk <volkeee1@gmail.com>
2018-03-14 00:20:53 +0200

    Work in progress. Testing the server app

--- CourseProjectServer/src/Debug.java
+++ CourseProjectServer/src/Debug.java
@@ -1,39 +1,35 @@
 import connection.SQLHelper;
-import dao.Program;
 import utils.Logger;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class Debug {
 
     public static void main(String[] args) {
         Logger.initialize();
-        Logger.info("Initialization", "Application started and log has been created in \"logs\" folder");
+        Logger.logInfo("Initialization", "Application started and log has been created in \"logs\" folder");
 
-        SQLHelper sqlHelper;
-        try {
-            sqlHelper = SQLHelper.getInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            sqlHelper = null;
-        }
-        ArrayList<Program> programs = new ArrayList<>();
+        Server.start();
 
-        try {
-            ResultSet resultSet = sqlHelper.getDataFor(new Program());
+        SQLHelper sqlHelper = null;
+//        try {
+//            sqlHelper = SQLHelper.getInstance();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        ArrayList<Program> programs = new ArrayList<>();
+//
+//        try {
+//            ResultSet resultSet = sqlHelper.getDataFor(Program.class);
+//
+//            while (resultSet.next()) {
+//                programs.add(Program.parse(resultSet));
+//            }
+//        } catch (SQLException | IllegalAccessException | InstantiationException e) {
+//            e.printStackTrace();
+//        }
+//
+//        for (Program program : programs) {
+//            System.out.println(program.toString());
+//        }
 
-            while (resultSet.next()) {
-                programs.add(Program.parse(resultSet));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        for (Program program : programs) {
-            System.out.println(program.toString());
-        }
     }
 }
