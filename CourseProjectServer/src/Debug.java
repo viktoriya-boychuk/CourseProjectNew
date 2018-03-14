deee23be34b738bc4e001908687d861aefe3b79a
Viktor Vovk <volkeee1@gmail.com>
2018-03-15 00:51:58 +0200

    Added multi-threading capabilities in main Debug class

--- CourseProjectServer/src/Debug.java
+++ CourseProjectServer/src/Debug.java
@@ -1,15 +1,33 @@
-import connection.SQLHelper;
+import dao.Program;
+import org.json.JSONObject;
 import utils.Logger;
 
-public class Debug {
+import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-    public static void main(String[] args) {
+public class Debug {
+    public static ArrayList<Runnable> mTaskPool;
+    public static ExecutorService mExecutorService;
+
+    public static void main(String[] args) throws InterruptedException {
+        mTaskPool = new ArrayList<>();
+        mExecutorService = Executors.newCachedThreadPool();
         Logger.initialize();
         Logger.logInfo("Initialization", "Application started and log has been created in \"logs\" folder");
 
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+
+            Logger.logInfo("Exit", "Application terminated");
+        }));
+
         Server.start();
 
-        SQLHelper sqlHelper = null;
+        handleTasks();
+
+//        SQLHelper sqlHelper = null;
+//        Server.finish();
 //        try {
 //            sqlHelper = SQLHelper.getInstance();
 //        } catch (Exception e) {
@@ -30,6 +48,23 @@ public class Debug {
 //        for (Program program : programs) {
 //            System.out.println(program.toString());
 //        }
+    }
 
+    private static void handleTasks() throws InterruptedException {
+        while (true) {
+            ArrayList<Runnable> toRemove = new ArrayList<>();
+            if (!mTaskPool.isEmpty()) {
+                for (Runnable runnable : mTaskPool) {
+                    addToTaskPool(runnable);
+                    toRemove.add(runnable);
+                    Thread.sleep(Server.DEFAULT_THREAD_DELAY);
+                }
+                mTaskPool.removeAll(toRemove);
+            }
+        }
+    }
+
+    public static void addToTaskPool(Runnable runnable) {
+        mTaskPool.add(runnable);
     }
 }
