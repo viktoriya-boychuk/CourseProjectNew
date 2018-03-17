ca2166a43e560afe72f8a7f4dcf327c21c89f133
Viktor Vovk <volkeee1@gmail.com>
2018-03-17 14:17:15 +0200

    Created TaskHandler class to work with stuff in the background

---
+++ Common/src/utils/TaskHandler.java
@@ -0,0 +1,87 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TaskHandler {
+    private static final long DEFAULT_THREAD_DELAY = 10;
+    private Looper mLooper;
+    private ExecutorService mExecutorService;
+    private ArrayList<Runnable> mTaskPool;
+    private Boolean mExit = false;
+
+    public TaskHandler(String threadName) {
+        mExecutorService = Executors.newCachedThreadPool();
+        mLooper = new Looper(threadName);
+        mTaskPool = new ArrayList<>();
+    }
+
+    public void startTask(Runnable runnable) {
+        mTaskPool.add(runnable);
+    }
+
+    private void addToTaskPool(Runnable runnable) {
+        mExecutorService.submit(runnable);
+    }
+
+    public void stop() {
+        Logger.logInfo("Thread stop", "Stopping" + getCurrentThreadName() + " thread");
+        mExit = true;
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mExecutorService.shutdown();
+    }
+
+    public void startInCurrentThread() {
+        mLooper.run();
+    }
+
+    public void startInBackgroundThread() {
+        mExecutorService.submit(mLooper);
+    }
+
+    public boolean status() {
+        return mExit;
+    }
+
+    public String getCurrentThreadName() {
+        return Thread.currentThread().getName();
+    }
+
+    private class Looper implements Runnable {
+        private String mThreadName;
+
+        public Looper(String mThreadName) {
+            this.mThreadName = mThreadName;
+        }
+
+        public String getmThreadName() {
+            return mThreadName;
+        }
+
+        @Override
+        public void run() {
+            while (!mExit) {
+                ArrayList<Runnable> toRemove = new ArrayList<>();
+                if (!mTaskPool.isEmpty()) {
+                    ArrayList<Runnable> buffer = new ArrayList<>(mTaskPool);
+                    for (Runnable runnable : buffer) {
+                        addToTaskPool(runnable);
+                        toRemove.add(runnable);
+                    }
+                    mTaskPool.removeAll(toRemove);
+                }
+                try {
+                    Thread.sleep(DEFAULT_THREAD_DELAY);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            Logger.logInfo("Thread stop", "Thread " + getCurrentThreadName() + " stopped");
+        }
+    }
+}
