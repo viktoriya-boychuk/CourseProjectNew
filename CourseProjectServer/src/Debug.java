c9e51073610862412d4a41ef96a5cabf326b5d7e
Viktor Vovk <volkeee1@gmail.com>
2018-03-17 14:18:14 +0200

    Shutdown hook has been rewritten

--- CourseProjectServer/src/Debug.java
+++ CourseProjectServer/src/Debug.java
@@ -1,56 +1,35 @@
 import connection.SQLHelper;
 import dao.Program;
-import org.json.JSONObject;
 import utils.Logger;
+import utils.TaskHandler;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Debug {
-    public static ArrayList<Runnable> mTaskPool;
-    public static ExecutorService mExecutorService;
+    private static TaskHandler mTaskHandler;
 
-    public static void main(String[] args) throws InterruptedException {
-        mTaskPool = new ArrayList<>();
-        mExecutorService = Executors.newCachedThreadPool();
+    public static void main(String[] args) {
+        mTaskHandler = new TaskHandler("MainApplication");
+
         Logger.initialize();
         Logger.logInfo("Initialization", "Application started and log has been created in \"logs\" folder");
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-
+            mTaskHandler.stop();
+            Server.finish();
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             Logger.logInfo("Exit", "Application terminated");
         }));
 
         Server.start();
 
-        handleTasks();
-    }
-
-    //TODO: Thread Handler class to avoid duplicate code
-    private static void handleTasks() throws InterruptedException {
-        while (true) {
-            ArrayList<Runnable> toRemove = new ArrayList<>();
-            if (!mTaskPool.isEmpty()) {
-                for (Runnable runnable : mTaskPool) {
-                    addToTaskPool(runnable);
-                    toRemove.add(runnable);
-                }
-                mTaskPool.removeAll(toRemove);
-            }
-            try {
-                Thread.sleep(Server.DEFAULT_THREAD_DELAY);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static void addToTaskPool(Runnable runnable) {
-        mExecutorService.submit(runnable);
+        mTaskHandler.startInCurrentThread();
     }
 
     private static void testSQLConnection() {
@@ -64,7 +43,7 @@ public class Debug {
         ArrayList<Program> programs = new ArrayList<>();
 
         try {
-            ResultSet resultSet = sqlHelper.getDataFor(Program.class);
+            ResultSet resultSet = sqlHelper.getResultSetFor(Program.class);
 
             //TODO: create static factory parser method
             while (resultSet.next()) {
