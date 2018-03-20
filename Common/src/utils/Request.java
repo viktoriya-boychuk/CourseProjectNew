6e5dc3e7774b472ca8ac35076bc84d4825e69345
Viktor Vovk <volkeee1@gmail.com>
2018-03-20 19:31:47 +0200

    Now Request class can work with different BaseDAO object in getData() method

--- Common/src/utils/Request.java
+++ Common/src/utils/Request.java
@@ -2,29 +2,45 @@ package utils;
 
 import dao.BaseDAO;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class Request {
-    enum Type {POST, GET, DELETE}
+    public enum RequestType {POST, GET, DELETE, UPDATE}
 
-    private Type mType;
+    private String KEY_TYPE = "Request-Type";
+    private String KEY_BODY = "Request-Body";
+
+    private RequestType mRequestType;
     private String mData;
+    private String mDataType;
 
-    public Request() {
+    private Request() {
     }
 
-    public Request(Type type, String data) {
-        this.mType = type;
+    public Request(RequestType type, String data) {
+        this.mRequestType = type;
         this.mData = data;
     }
 
-    public Type getType() {
-        return mType;
+    public Request(RequestType type, ArrayList<BaseDAO> data) {
+        this.mRequestType = type;
+        this.addData(data);
     }
 
-    public void setType(Type mType) {
-        this.mType = mType;
+    public Request(RequestType requestType, Class<? extends BaseDAO> forClass) {
+        this.mRequestType = requestType;
+        this.mDataType = forClass.getName();
+    }
+
+    public RequestType getType() {
+        return mRequestType;
+    }
+
+    public void setType(RequestType mType) {
+        this.mRequestType = mType;
+
     }
 
     public String getBody() {
@@ -35,13 +51,42 @@ public class Request {
         this.mData = mBody;
     }
 
-    public void addData(ArrayList<BaseDAO> baseDAO) {
+    public String getDataType() {
+        return mDataType;
+    }
+
+    public void setDataType(String mDataType) {
+        this.mDataType = mDataType;
+    }
+
+    public void addData(ArrayList<BaseDAO> baseDAOList) {
+        JSONObject jsonObject = new JSONObject();
         JSONArray jsonArray = new JSONArray();
-        
-        for(BaseDAO object : baseDAO) {
+
+        for (BaseDAO object : baseDAOList) {
             jsonArray.put(object.toJSON());
         }
-        
-        this.setBody(jsonArray.toString());
+        jsonObject.put(baseDAOList.get(0).getObjectName(), jsonArray);
+
+        this.setBody(jsonObject.toString());
+    }
+
+    @Override
+    public String toString() {
+        return new JSONObject().put(KEY_TYPE, this.mRequestType.toString()).put(KEY_BODY, mData).toString();
+    }
+
+    public ArrayList<BaseDAO> getData() {
+        ArrayList<BaseDAO> dataList = new ArrayList<>();
+        JSONArray jsonArray = new JSONArray(new JSONObject(mData).get(mDataType));
+        for(int i = 0; i>jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            try {
+                BaseDAO object = ((BaseDAO) Class.forName(this.getDataType()).newInstance()).parseJSON(jsonObject);
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
+        return dataList;
     }
 }
