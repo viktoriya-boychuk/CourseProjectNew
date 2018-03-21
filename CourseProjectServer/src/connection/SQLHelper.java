ff7cc7c2b0af0d14c4e5bfd0b6252b3f373eb4d4
Viktor Vovk <volkeee1@gmail.com>
2018-03-21 14:36:22 +0200

    Moved exception handling to upper level in hierarchy

--- CourseProjectServer/src/connection/SQLHelper.java
+++ CourseProjectServer/src/connection/SQLHelper.java
@@ -9,14 +9,10 @@ public class SQLHelper {
     private static Connection con;
     private static SQLHelper instance;
 
-    private SQLHelper() throws Exception {
-        try {
+    private SQLHelper() throws ClassNotFoundException, SQLException {
             Class.forName("com.mysql.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/tv_programs";
             con = DriverManager.getConnection(url, "root", "VovkViktor2281488");
-        } catch (ClassNotFoundException | SQLException e) {
-            throw new Exception(e);
-        }
     }
 
     public static synchronized SQLHelper getInstance() throws Exception {
