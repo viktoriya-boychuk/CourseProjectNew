7eb9a7f3a60cb81bcc8415d4f536df5b8d78fbde
volkeee <volkeee1@gmail.com>
2018-04-02 02:44:18 +0300

    RequestHandler has been moved away from it's original location

---
+++ CourseProjectServer/src/RequestHandler.java
@@ -0,0 +1,54 @@
+import connection.SQLHelper;
+import dao.BaseDAO;
+import utils.Logger;
+import utils.Request;
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+public class RequestHandler implements Callable<ArrayList<BaseDAO>> {
+    private Request mRequest;
+    private Socket mSocket;
+    private Future<ArrayList<BaseDAO>> mFuture;
+
+    public RequestHandler(Request request, Socket socket) {
+        this.mRequest = request;
+        this.mSocket = socket;
+    }
+
+    public void setFuture(Future<ArrayList<BaseDAO>> mFuture) {
+        this.mFuture = mFuture;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public ArrayList<BaseDAO> call() {
+        switch (mRequest.getRequestType()) {
+            case GET:
+                Task task = new Task(mFuture, mSocket, mRequest);
+                Server.addToWaitingList(task);
+
+                try {
+                    return SQLHelper.getInstance().getArrayListFor((Class<? extends BaseDAO>) Class.forName(mRequest.getDataType()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case POST:
+                Logger.logInfo("POST", "This is post request");
+                break;
+            case DELETE:
+                Logger.logInfo("DELETE", "This is delete request");
+                break;
+            case UPDATE:
+                Logger.logInfo("UPDATE", "This is update request");
+                break;
+            default:
+                Logger.logInfo("NO-TYPE", "This request got no type");
+                break;
+        }
+        return new ArrayList<>();
+    }
+}
